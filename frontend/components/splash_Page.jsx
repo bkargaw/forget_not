897b62c4162d698fe5e029c94b2b9f28671c8e08
Bruk Argaw <bkargaw@gmail.com>
2017-03-17 08:34:12 -0700

    added splash image

--- frontend/components/splash_Page.jsx
+++ frontend/components/splash_Page.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const spalshPage=  () =>(
   <div>
-    <h1>this is a splashPage</h1>
+    <img
+      src='http://res.cloudinary.com/dwuzkffrk/image/upload/g_face,q_100,r_0,x_696,y_407/v1489761831/busybackground_henq1f.jpg'
+      className='mainSplashPageImage'></img>
   </div>
 );
 
