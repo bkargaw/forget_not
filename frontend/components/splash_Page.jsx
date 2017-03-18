aebe79c4fa5b7ce278280a3ee54b3e53935f0480
Bruk Argaw <bkargaw@gmail.com>
2017-03-17 17:28:43 -0700

    added footer links

--- frontend/components/splash_Page.jsx
+++ frontend/components/splash_Page.jsx
@@ -9,11 +9,11 @@ const spalshPage=  () =>(
         <button>GET STARTED</button>
     </section>
     <section className='splashPageFooter'>
-      <div Source>
-      <img src='http://res.cloudinary.com/dwuzkffrk/image/upload/c_scale,w_71/v1489792533/git_img_tfbjbn.png'/>
-      <a   href="https://github.com/bkargaw/forget_not">Source</a>
+      <div className='Source'>
+        <img src='http://res.cloudinary.com/dwuzkffrk/image/upload/c_scale,w_71/v1489792533/git_img_tfbjbn.png'/>
+        <a   href="https://github.com/bkargaw/forget_not">Source</a>
       </div>
-      <div Creator>
+      <div className= 'Creator'>
         <img src='http://res.cloudinary.com/dwuzkffrk/image/upload/c_scale,w_68/v1489792416/Linkedin-PNG-Picture_btid2r.png'/>
       <a  className='Creator' href="https://www.linkedin.com/in/bruk-argaw-56b0039a/">About The Creator</a>
       </div>
