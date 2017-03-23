267173e9d7c4316d015e61159ea2f1480fd14dce
Bruk Argaw <bkargaw@gmail.com>
2017-03-23 06:36:46 -0700

    added splash page footer css

--- frontend/components/splash_Page.jsx
+++ frontend/components/splash_Page.jsx
@@ -21,7 +21,6 @@ class spalshPage extends React.Component {
       //.. all what you put in here you will get access in
       // the modal props ;)
       changeform: () => {
-
                     this.addModal(oppsitButton);
                   }
     })
@@ -38,12 +37,12 @@ class spalshPage extends React.Component {
         </section>
         <section className='splashPageFooter'>
           <div className='Source'>
-            <img src='http://res.cloudinary.com/dwuzkffrk/image/upload/c_scale,w_71/v1489792533/git_img_tfbjbn.png'/>
-            <a   href="https://github.com/bkargaw/forget_not">Source</a>
+            <i className="fa fa-github" aria-hidden="true"></i>
+            <a  href="https://github.com/bkargaw/forget_not">Source</a>
           </div>
           <div className= 'Creator'>
-            <img src='http://res.cloudinary.com/dwuzkffrk/image/upload/c_scale,w_68/v1489792416/Linkedin-PNG-Picture_btid2r.png'/>
-          <a  className='Creator' href="https://www.linkedin.com/in/bruk-argaw-56b0039a/">About The Creator</a>
+            <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+            <a href="https://www.linkedin.com/in/bruk-argaw-56b0039a/">About The Creator</a>
           </div>
       </section>
       </div>
