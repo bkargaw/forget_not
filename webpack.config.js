efc11847e91bca5da0921b1c833682b0817a9ac8
Bruk Argaw <bkargaw@gmail.com>
2017-04-22 16:54:30 -0700

    changed the css loader in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,17 @@ module.exports = {
           presets: ['es2015', 'react']
         }
       },
-      { test: /\.css$/, loader: "style-loader!css-loader?importLoaders=1" },
-      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+      {
+        test: /\.css$/,
+        include: [
+            path.resolve(__dirname, "not_exist_path")
+        ],
+        loader: "style!css"
+      },
+      {
+          test: /\.(png|jpg|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'url?limit=512&&name=[path][name].[ext]?[hash]'
+      }
     ]
   },
   devtool: 'source-maps',
